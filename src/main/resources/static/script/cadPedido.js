b1d2973a128a117ecb2925cbd620597da9ff24e7
Erick Borges dos Santos <erickborges04@hotmail.com>
2025-02-13 17:25:28 -0300

    POST e DELETE funcionando

--- src/main/resources/static/script/cadPedido.js
+++ src/main/resources/static/script/cadPedido.js
@@ -2,6 +2,7 @@ const formulario = document.querySelector("form");
 const Icliente = document.querySelector("#txtCliente");
 const Iitens = document.querySelector("#txtItens");
 const Ivalor = document.querySelector("#txtValor");
+const Iid = document.querySelector("#idSeletor");
 
 function cadastrarPedido() {
     fetch("http://localhost:8080/api/pedidos",
@@ -12,12 +13,31 @@ function cadastrarPedido() {
             },
             method: "POST",
             body: JSON.stringify({
-                nome: Icliente.value,
+                cliente: Icliente.value,
                 itens: Iitens.value,
                 valor: Ivalor.value, 
             })
         })
         .then(function(res) {console.log(res)})
+        .catch(function(res) {console.log(res)})
+};
+
+function deletarPedido() {
+    const id = Iid.value;
+
+    if(!id){
+        alert("ID vazio ou não encontrado!")
+        return;
+    }
+
+    fetch(`http://localhost:8080/api/pedidos/${id}`,
+        {
+            method: "DELETE"
+        })
+        .then(function(res) {
+            console.log(res)
+            alert(`Pedido ${id} excluido com sucesso`)
+        })
         .catch(function(res) {console.log(res)})
 };
 
